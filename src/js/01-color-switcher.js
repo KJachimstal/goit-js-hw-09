68a2a62fd0196dda532b96d0211cbe3d1fab5f3a
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-17 19:25:39 +0100

    Added getRandomHexColor function

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
